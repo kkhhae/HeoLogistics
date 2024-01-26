81f9b08a0b6369ee102e30d12055edbac8969fd7
kkhhae <rlagodh3662@gmail.com>
2024-01-26 20:11:15 +0900

    프로젝트 초기파일

---
+++ src/main/java/com/heologistics/HeologisticsApplication.java
@@ -0,0 +1,13 @@
+package com.heologistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeologisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeologisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heologistics/HeologisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heologistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeologisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
