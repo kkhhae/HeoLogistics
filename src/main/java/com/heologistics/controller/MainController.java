3e7b121be70152cc9970da3071726ea95fb240d3
kkhhae <rlagodh3662@gmail.com>
2024-02-05 16:03:22 +0900

    404추가 및 기타운송 처리

--- src/main/java/com/heologistics/controller/MainController.java
+++ src/main/java/com/heologistics/controller/MainController.java
@@ -8,20 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String Main(){
+    public String Main() {
         return "index.html";
     }
 
     @GetMapping("/normalDelivery")
-    public String normalDelivery(){
+    public String normalDelivery() {
         return "normalDelivery.html";
     }
+
     @GetMapping("/quickDelivery")
-    public String quickDelivery(){
+    public String quickDelivery() {
         return "quickDelivery.html";
     }
+
     @GetMapping("/hugeDelivery")
-    public String hugeDelivery(){
+    public String hugeDelivery() {
         return "hugeDelivery.html";
     }
 }
