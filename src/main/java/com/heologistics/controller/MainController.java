abb91b7a3bea00f8ddb66cc10c9bfd702141e9be
kkhhae <rlagodh3662@gmail.com>
2024-03-05 23:41:20 +0900

    페이지넘어가기 완. 데이터가 안넘어옴

--- src/main/java/com/heologistics/controller/MainController.java
+++ src/main/java/com/heologistics/controller/MainController.java
@@ -5,6 +5,7 @@ import com.heologistics.entity.Receipt;
 import com.heologistics.repository.DelivetyRepository;
 import com.heologistics.service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,21 +70,11 @@ public class MainController {
     }
 
     @PostMapping(value = "/requestComplete", consumes = "application/json")
-    public String requestAction(@RequestBody Receipt receiptData, Model model) {
+    public ResponseEntity<String> requestAction(@RequestBody Receipt receiptData, Model model) {
         delivetyRepository.save(receiptData);
-        try {
-            String carType = URLEncoder.encode(receiptData.getCarType(), StandardCharsets.UTF_8.toString());
-            String price = URLEncoder.encode(receiptData.getPrice(), StandardCharsets.UTF_8.toString());
-            String distance = URLEncoder.encode(receiptData.getDistance(), StandardCharsets.UTF_8.toString());
-            String deliveryAddress = URLEncoder.encode(receiptData.getDeliveryAddress(), StandardCharsets.UTF_8.toString());
-            String deliveryDate = URLEncoder.encode(receiptData.getDeliveryDate(), StandardCharsets.UTF_8.toString());
-            String toll = URLEncoder.encode(receiptData.getToll(), StandardCharsets.UTF_8.toString());
-            String duration = URLEncoder.encode(receiptData.getDuration(), StandardCharsets.UTF_8.toString());
+        return ResponseEntity.ok().body("{\"redirect\": \"/requestComplete?carType=" + receiptData.getCarType() + "&price=" + receiptData.getPrice() + "&distance=" + receiptData.getDistance() + "&deliveryAddress=" + receiptData.getDeliveryAddress() + "&deliveryDate=" + receiptData.getDeliveryDate() + "&toll=" + receiptData.getToll() + "&duration=" + receiptData.getDuration() + "\"}");
 
-            return "redirect:/requestComplete.html?carType=" + carType + "&price=" + price + "&distance=" + distance + "&deliveryAddress=" + deliveryAddress + "&deliveryDate=" + deliveryDate + "&toll=" + toll + "&duration=" + duration;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+//        return ResponseEntity.ok().body("{\"redirect\": \"/requestComplete.html?carType=" + receiptData.getCarType() + "&price=" + receiptData.getPrice() + "&distance=" + receiptData.getDistance() + "&deliveryAddress=" + receiptData.getDeliveryAddress() + "&deliveryDate=" + receiptData.getDeliveryDate() + "&toll=" + receiptData.getToll() + "&duration=" + receiptData.getDuration() + "\"}");
     }
+
 }
